7c259275129f914c6d9e710b8598f8cec2257b84
hebraheem <iadebiyi01@gmail.com>
2021-01-25 13:03:16 +0100

    added public routing

--- src/App.js
+++ src/App.js
@@ -9,24 +9,34 @@ import MovieInfo from "./components/MovieInfo";
 import { Route, Switch } from "react-router-dom";
 import PrivateRoute from './components/PrivateRoute'
 import ResetPass from "./components/ResetPass";
-//import {useMovieConsumer} from './context'
+import {useMovieConsumer} from './context'
+import PublicRoute from "./components/PublicRoute";
+
 
 function App() {
-  //const classes = useStyles();
-  //const {currentUser} = useMovieConsumer();
+  const {currentUser} = useMovieConsumer();
   return (
     <>
       <div>
-         <Navbar />
+        <Navbar />
         <Switch>
-          <PrivateRoute exact path="/" component={Movies} />
-          <PrivateRoute path="/movie_info" component={MovieInfo} />
-          <Route exact path="/home" component={Home} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/login" component={LogIn} />
-          <Route path="/reset_pass" component={ResetPass} />
+          <PrivateRoute
+            exact
+            path="/"
+            component={Movies}
+            currentUser={currentUser}
+          />
+          <PrivateRoute
+            path="/movie_info"
+            component={MovieInfo}
+            currentUser={currentUser}
+          />
+          <PublicRoute exact path="/home" component={Home} />
+          <PublicRoute path="/signup" component={SignUp} />
+          <PublicRoute path="/login" component={LogIn} />
+          <PublicRoute path="/reset_pass" component={ResetPass} />
         </Switch>
-      </div>   
+      </div>
       <CssBaseline />
     </>
   );
